9ecb09678f2ce6a0278855bfab1203decd355b7f
Roque Ocampo <roko.okmpo@gmail.com>
2013-04-29 20:36:02 -0400

    Creando directorio del proyecto

---
+++ ventas/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ventas::Application.config.secret_token = '04cf5a8a46bed4ff98ce1e979e2156e989b999cf6dd28cc95e37ca418f282624a4a85c3fc9af6e3db0c60c9eb8df093e8e5f9befc21f1ac4a6c5a34a287fd663'
