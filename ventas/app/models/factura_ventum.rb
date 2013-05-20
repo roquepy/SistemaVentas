bc9d8f33c3d54476acbbb273538a849982d68b98
diego <diego.10989@gmail.com>
2013-05-20 16:03:03 +0200

    Actualizacion de Factura Venta detalle y Cabecera
    #19,#20: Se han actualizado dichas vistas

--- ventas/app/models/factura_ventum.rb
+++ ventas/app/models/factura_ventum.rb
@@ -1,5 +1,5 @@
 class FacturaVentum < ActiveRecord::Base
-	attr_accessible :id_cliente,:id_condicion_pago,:id_tipo_valor,:id_funcionario,:anulado,:monto_total,:fecha,:nro_factura,:total_descuento,:total_iva5,:total_iva10,:total_iva
+	attr_accessible :id_cliente,:id_condicion_pago,:id_tipo_valor,:id_funcionario,:monto_total,:fecha,:nro_factura,:total_descuento,:total_iva5,:total_iva10,:total_iva
     belongs_to :cliente, :foreign_key=>"id_cliente"
     belongs_to :condicion_de_pago, :foreign_key=>"id_condicion_pago"
     belongs_to :tipo_valor_pago, :foreign_key=>"id_tipo_valor"
@@ -15,4 +15,43 @@ class FacturaVentum < ActiveRecord::Base
     	return factura_nro
     	
     end
+     def  self.descuento(nrofactura)
+        descuento=where ('SELECT SUM(((p.precio_unitario*fd.cantidad)* fd.descuento)/100)
+          FROM factura_venta AS fv INNER JOIN detalle_factura_venta AS 
+          fd ON fv.id=fd.id_factura_venta INNER JOIN productos AS p ON fd.id_producto=p.id 
+          WHERE fv.nro_factura="%#{nro_factura}%"')
+        if  descuento.blank?
+             descuento=0.00
+        end 
+        return  descuento   
+    end
+
+     def  self.factura_total_iva(nro_factura)
+        total_iva=where ('SELECT SUM(p.precio_unitario*fd.cantidad)  
+            FROM factura_venta AS fv INNER JOIN detalle_factura_venta 
+            AS fd ON fv.id=fd.id_factura_venta INNER JOIN productos AS p 
+            ON fd.id_producto=p.id WHERE fv.nro_factura="%#{nro_factura}%"')
+        if total_iva.blank?
+            total_iva=0.00
+        end 
+        return total_iva-descuento(nro_factura)
+        
+    end
+
+     def  self.factura_total_iva10(nrofactura)
+        total_iva10=(factura_total_iva(nrofactura)*10)/100
+         if total_iva10.blank?
+            total_iva10=0.00
+        end 
+        return total_iva10
+    end
+    def  self.factura_total_iva5(nro_factura)
+        total_iva5=(factura_total_iva(nro_factura)*5)/100
+         if total_iva5.blank?
+            total_iva5=0.00
+        end 
+        return total_iva5
+    end
+
+
 end
--- ventas/db/migrate/20130517145147_create_factura_venta.rb
+++ ventas/db/migrate/20130517145147_create_factura_venta.rb
@@ -1,12 +1,11 @@
 class CreateFacturaVenta < ActiveRecord::Migration
   def change
     create_table :factura_venta do |t|
-        t.date :fecha,  :null => false
+       t.date :fecha,  :null => false
        t.integer :id_cliente,  :null => false
        t.integer :id_condicion_pago,  :null => false
        t.integer :id_tipo_valor,  :null => false
        t.integer :id_funcionario,  :null => false
-       t.string :anulado,  :null => false
        t.integer :nro_factura,  :null => false
        t.decimal :total_descuento,  :null => false
        t.decimal :total_iva5,  :null => false
