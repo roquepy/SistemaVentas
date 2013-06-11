1735825489e0fd74fab5323593b96a9b8ce608b9
Lizza Lorena Lopez Maciel <lizza.skr08@gmail.com>
2013-06-11 12:57:17 -0400

    Corrección de CSS y Validaciones
    Tareas #14 y #15: Corrección/Actualización de CSS y Validaciones en los
    ABM de Usuarios y Funcionarios.

--- ventas/db/migrate/20130505185507_create_funcionarios.rb
+++ ventas/db/migrate/20130505185507_create_funcionarios.rb
@@ -1,14 +1,14 @@
 class CreateFuncionarios < ActiveRecord::Migration
   def change
     create_table :funcionarios do |t|
-     t.string :num_identidad
+     t.string :num_identidad,:limit => 15,:null => false
      t.string :nombres, :limit => 50, :null => false
      t.string :apellidos,:limit => 50, :null => false
      t.string :direccion, :limit => 70,:null => true
-     t.string :telefono, :limit => 10, :null => false
-     t.string :estado_civil, :limit => 10,:null => false
+     t.string :telefono, :limit => 15, :null => false
+     t.string :estado_civil, :limit => 8,:null => false
      t.timestamps :fecha
-     t.string :sexo, :limit => 15,:null => false
+     t.string :sexo, :limit => 1,:null => false
      t.integer :id_localidad, :null => false
      t.timestamps :fecha
      t.integer :id_estado_funcionario, :null => false
