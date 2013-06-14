818844a637b3a6f9e0625c678faa38b6adcfbca9
diego <diego.10989@gmail.com>
2013-06-14 15:39:38 +0200

    Actualizacion de Factura Venta Cabecera y Detalle
    #19,#20: Se ha actualizado la cabecera y el detalle de la factura

--- ventas/app/models/detalle_factura_ventum.rb
+++ ventas/app/models/detalle_factura_ventum.rb
@@ -1,5 +1,6 @@
 class DetalleFacturaVentum < ActiveRecord::Base
    attr_accessible :id_factura_venta,:id_producto,:cantidad,:descuento
    belongs_to :factura_ventum, :foreign_key=>"id_factura_venta"
+    has_and_belongs_to_many :factura_ventum, join_table: :detalle_factura_venta
    belongs_to :producto, :foreign_key=>"id_producto"
 end
