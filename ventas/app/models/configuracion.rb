713e2a8619fab5bde549ef5dfb03fe9a7213b633
Roque Ocampo <roko.okmpo@gmail.com>
2013-06-16 23:34:07 -0400

    Actualizacion ABM Configuracion Empresa
    Issue #27

--- ventas/app/models/configuracion.rb
+++ ventas/app/models/configuracion.rb
@@ -1,4 +1,13 @@
 class Configuracion < ActiveRecord::Base
-	attr_accessible :nombre_logo, :logo
+	attr_accessible :nombre_logo, :logo, :nombre_empresa, :direccion, :telefono, :telefono_auxiliar, :ruc, :timbrado, :timbrado_vencimiento
    mount_uploader :logo, ImageUploader
+   validates :nombre_logo, :logo, :nombre_empresa, :direccion, :telefono, :ruc, :timbrado, :timbrado_vencimiento, :presence => true
+   validates :nombre_logo, :length => { :minimum => 2}
+   validates :nombre_empresa, :length => { :minimum => 4}
+   validates :direccion, :length => { :minimum => 5}
+   validates :telefono, :length => { :minimum => 6}
+   validates :telefono_auxiliar, :length => { :minimum => 6}
+   validates :ruc, :length => { :minimum => 6}
+   validates :timbrado, :length => { :minimum => 6}
+   validates :timbrado_vencimiento, :length => { :minimum => 6}
 end
