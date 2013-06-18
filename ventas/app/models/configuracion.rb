5fc278ed578bcf1c416abe3ba649e379c9458278
Roque Ocampo <roko.okmpo@gmail.com>
2013-06-18 10:52:42 -0400

    Validaciones ABM Datos de la Empresa
    Issue #27

--- ventas/app/models/configuracion.rb
+++ ventas/app/models/configuracion.rb
@@ -2,12 +2,12 @@ class Configuracion < ActiveRecord::Base
 	attr_accessible :nombre_logo, :logo, :nombre_empresa, :direccion, :telefono, :telefono_auxiliar, :ruc, :timbrado, :timbrado_vencimiento
    mount_uploader :logo, LogoUploader
    validates :nombre_logo, :nombre_empresa, :direccion, :telefono, :ruc, :timbrado, :timbrado_vencimiento, :presence => true
-   validates :nombre_logo, :length => { :minimum => 2}
-   validates :nombre_empresa, :length => { :minimum => 4}
-   validates :direccion, :length => { :minimum => 5}
-   validates :telefono, :length => { :minimum => 6}
+   validates :nombre_logo, :length => { :minimum => 2}, :format => { :with => /\A[a-zA-Z\s]+\z/ }
+   validates :nombre_empresa, :length => { :minimum => 4}, :format => { :with => /\A[a-zA-Z\s]+\z/ }
+   validates :direccion, :length => { :minimum => 5}, :format => { :with => /\A[a-zA-Z\s\d\.\/]+\z/}
+   validates :telefono, :length => { :minimum => 6}, :format => { :with =>  /^[0-9]{3,4}-? ?[0-9]{5,6}$/ }
    validates :telefono_auxiliar, :format => { :with =>  /^[0-9]{3,4}-? ?[0-9]{5,6}$/ }
-   validates :ruc, :length => { :minimum => 6}
-   validates :timbrado, :length => { :minimum => 6}
+   validates :ruc, :length => { :minimum => 6}, :format => { :with => /\d\-/}
+   validates :timbrado, :length => { :minimum => 6}, :format => { :with => /\d/}
    validates :timbrado_vencimiento, :length => { :minimum => 8}, :format => { :with => /\A\d{2}(\/|-)\d{2}(\/|-)\d{4}\Z/i }
 end
