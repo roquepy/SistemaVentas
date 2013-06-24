c5677ff2be794ada8c0869a6f71c0f7d36fda311
Roque Ocampo <roko.okmpo@gmail.com>
2013-06-24 19:57:08 -0400

    Control de Foreign Key de las Tablas
    Issue #44

---
+++ ventas/db/migrate/20130624224819_add_foreign_key_tables.rb
@@ -0,0 +1,16 @@
+class AddForeignKeyTables < ActiveRecord::Migration
+  def change
+  	add_foreign_key(:clientes, :localidads, column: 'id_localidad')
+  	add_foreign_key(:localidads, :departamentos, column: 'id_departamento')
+  	add_foreign_key(:funcionarios, :localidads, column: 'id_localidad')
+  	add_foreign_key(:funcionarios, :estados_funcionarios, column: 'id_estado_funcionario')
+  	add_foreign_key(:funcionarios, :funcions, column: 'id_funcion')
+  	add_foreign_key(:users, :funcionarios, column: 'id_funcionario')
+  	add_foreign_key(:factura_venta, :clientes, column: 'id_cliente')
+  	add_foreign_key(:factura_venta, :condicion_de_pagos, column: 'id_condicion_pago')
+  	add_foreign_key(:factura_venta, :tipo_valor_pagos, column: 'id_tipo_valor')
+  	add_foreign_key(:factura_venta, :funcionarios, column: 'id_funcionario')
+  	add_foreign_key(:detalle_factura_venta, :factura_venta, column: 'id_factura_venta')
+  	add_foreign_key(:detalle_factura_venta, :productos, column: 'id_producto')
+  end
+end
