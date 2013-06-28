e20d2cb10cda568fc6a69d869f0671a0e5a00c1d
diego <diego.10989@gmail.com>
2013-06-28 13:17:22 +0200

    Actualizacion de  Factura Venta Detalle
    #20: Se ha actualizado el detalle de la Factura Venta

--- ventas/db/migrate/20130528180011_create_configuracions.rb
+++ ventas/db/migrate/20130528180011_create_configuracions.rb
@@ -2,7 +2,6 @@ class CreateConfiguracions < ActiveRecord::Migration
   def change
     create_table :configuracions do |t|
     	t.string :nombre_logo, :limit => 20, :null =>false
-    	t.string :logo, :null => false
     	t.string :nombre_empresa,:limit => 30,:null => false
     	t.string :direccion,:limit => 30,:null => false
     	t.string :ruc,:limit =>10, :null => false
--- ventas/db/migrate/20130529214552_add_image_to_configuracions.rb
+++
@@ -1,6 +0,0 @@
-class AddImageToConfiguracions < ActiveRecord::Migration
-  def change
-    add_column :configuracions, :logo, :string
-
-  end
-end
