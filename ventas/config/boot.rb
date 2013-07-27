8552c2cf00979092678d237e58f93522839b11a7
Roque Ocampo <roko.okmpo@gmail.com>
2013-07-27 19:04:46 -0400

    I18n Español

--- ventas/config/boot.rb
+++ ventas/config/boot.rb
@@ -5,3 +5,4 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 #RAILS_ROOT = "#{File.dirname(FILE)}/.." unless defined?(RAILS_ROOT)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
