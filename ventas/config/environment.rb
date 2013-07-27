2f7f34b34412e2c2d64360765aa7bbd16a34900d
Roque Ocampo <roko.okmpo@gmail.com>
2013-07-27 19:51:56 -0400

    Modificar Archivo de I18n

--- ventas/config/environment.rb
+++ ventas/config/environment.rb
@@ -3,7 +3,7 @@ require File.expand_path('../application', __FILE__)
 Ventas::Application.configure  do
 	config.gem 'calendar_date_select'
 	config.gem 'simple_audit'
-	config.i18n.default_locale = :es
+	#config.i18n.default_locale = :es
 end
 
 # Initialize the rails application
